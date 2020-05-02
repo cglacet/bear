e728f3f387699de9cdb22bb1f72de1761768bc08
Christian Glacet <cglacet@kune.tech>
2020-05-02 18:38:04 +0200

    Add auto locate for sqlite DB

--- constants.py
+++ constants.py
@@ -1,10 +1,21 @@
 import os
+import search_file
+
+
+# If it fails and you struggle finding the location of your , try to run:
+#   find ~ -iname database.sqlite | grep bear
+MANUALLY_SET_DB_LOCATION = ""
 
 HOME = os.getenv('HOME', '')
-SOME_RANDOM_ID = "9K33E3U3T4"
-# If you struggle finding the location of your , try to run:
-#   find ~ -iname database.sqlite | grep bear
-BEAR_DB = os.path.join(HOME, f'Library/Group Containers/{SOME_RANDOM_ID}.net.shinyfrog.bear/Application Data/database.sqlite')
+LIBRARY = os.path.join(HOME, 'Library')
+BEAR_DB = search_file.find_first(r'.*bear.*database\.sqlite$', LIBRARY)
+
+if not BEAR_DB:
+    print(
+        f"\n\n!!! ERROR: Couldn't locate Bear app database, please edit {__name__}.py"
+        f"file to set MANUALLY_SET_DB_LOCATION this manually !!!\n\n"
+    )
+    exit(1)
 
 WRITE_API_URL = "bear://x-callback-url/add-text"
 
