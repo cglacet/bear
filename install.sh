e8aa9c367d8573c641f6c387d693d4ecb78971f3
Christian Glacet <cglacet@kune.tech>
2020-05-06 20:32:15 +0200

    Bug in installation path

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ url="https://raw.githubusercontent.com/cglacet/install-scripts/master/osx/git-cl
 /bin/bash -c "$(curl -fsSL $url)" bash $install_dir $git_repo
 
 url="https://raw.githubusercontent.com/cglacet/install-scripts/master/osx/install-python.sh?$(date +%s)"
-/bin/bash -c "$(curl -fsSL url)"
+/bin/bash -c "$(curl -fsSL $url)"
 
 echo "Adding back-reference links to your ${red}Bear${nc} notes"
 echo "----------------------------------------------"
