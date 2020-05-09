ed17cc3cbf3174755ca6a03bb629d7db9d025d45
Christian Glacet <cglacet@kune.tech>
2020-05-09 04:43:45 +0200

    Installation of recent python using pyenv

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
-
+#!/bin/bash
 install_dir=~/.github/bear/back-references
 git_repo=git@github.com:cglacet/bear.git
 
 url="https://raw.githubusercontent.com/cglacet/install-scripts/master/osx/git-clone.sh?$(date +%s)"
 /bin/bash -c "$(curl -fsSL $url)" bash $install_dir $git_repo
 
-url="https://raw.githubusercontent.com/cglacet/install-scripts/master/osx/install-python.sh?$(date +%s)"
+url="https://raw.githubusercontent.com/cglacet/install-scripts/master/osx/install-python_gt_3.6.sh?$(date +%s)"
 /bin/bash -c "$(curl -fsSL $url)"
 
 echo "Adding back-reference links to your ${red}Bear${nc} notes"
